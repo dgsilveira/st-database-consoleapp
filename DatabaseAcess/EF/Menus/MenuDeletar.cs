71ebab263d77aabb088fd5f432707e7c08145e38
Douglas Silveira <douglasgsilveira@gmail.com>
2024-06-24 22:32:02 -0300

    EF

---
+++ DatabaseAcess/EF/Menus/MenuDeletar.cs
@@ -0,0 +1,26 @@
+﻿using EF.Banco;
+
+namespace EF.Menus
+{
+    internal class MenuDeletar : Menu
+    {
+        public override void Executar(Contexto contexto)
+        {
+            base.Executar(contexto);
+            ExibirTituloDaOpcao("Deletar");
+
+            Console.Write("Digite o id: ");
+            int id = int.Parse(Console.ReadLine()!);
+
+            var pessoa = contexto.Pessoas.First(p => p.Id == id);
+
+            contexto.Pessoas.Remove(pessoa);
+            contexto.SaveChanges();
+
+            Console.WriteLine($"Deletado com sucesso!");
+            Console.WriteLine($"Aguarde, vamos retornar ao menu principal automaticamente!");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+    }
+}
---
+++ DatabaseAcess/EF/Menus/MenuRegistrar.cs
@@ -0,0 +1,37 @@
+﻿using EF.Banco;
+using EF.Model;
+
+namespace EF.Menus
+{
+    internal class MenuRegistrar : Menu
+    {
+        public override void Executar(Contexto contexto)
+        {
+            base.Executar(contexto);
+            ExibirTituloDaOpcao("Registrar novo");
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine()!;
+            Console.Write("Digite a letra que representa o gênero: ");
+            string genero = Console.ReadLine()!.ToUpper()!;
+            Console.WriteLine("Digite a data de nascimento no formato dd/MM/yyyy");
+            string stringDataNascimento = Console.ReadLine()!;
+            DateTime dataNascimento = DateTime.ParseExact(stringDataNascimento, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+
+            var pessoa = new Pessoa()
+            {
+                Ativo = true,
+                DataModificacao = DateTime.Now,
+                DataNascimento = dataNascimento,
+                Genero = genero,
+                Nome = nome
+            };
+
+            contexto.Pessoas.Add(pessoa);
+            contexto.SaveChanges();
+            Console.WriteLine($"Registrado com sucesso!");
+            Console.WriteLine($"Aguarde, vamos retornar ao menu principal automaticamente!");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+    }
+}
